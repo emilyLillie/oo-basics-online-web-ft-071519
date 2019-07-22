23059b2a880fa0f85a94e7123e20ae8501e70318
Emily Lillie <emilylillie3@gmail.com>
2019-07-22 21:18:33 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -6,6 +6,32 @@ class Book
   def title 
     @title
   end
+  
+  def author=(author)
+    @author = author
+  end
+  
+  def author
+    @author
+  end
+  
+  def page_count=(num)
+    @page_count
+  end
+  
+  def page_count
+    @page_count
+  end
+  
+  def genre=(genre)
+    @genre = genre
+  end 
+  
+  def genre
+    @genre
+  end 
+  
+  
 end
 
 
